80c230a140aa9a34a00203d00d7e37abf7c15c17
Jude Lawrence <judesmail@gmail.com>
2022-12-09 21:32:32 +0400

    Day 7 update.

--- day07.rb
+++ day07.rb
@@ -1,7 +1,5 @@
 file = File.open('day07-input.txt')
 
-
-
 file_system = {}
 dir_sizes = Hash.new { |h, k| h[k] = 0 }
 
@@ -12,28 +10,28 @@ file.readlines.each do |line|
   when '$'
     case line.split[1]
     when 'cd'
-      puts 'change directory'
+    #   puts 'change directory'
       if line.split(' ').last != '..'
         current_dir << line.split(' ').last
       else
         current_dir.pop
       end
     when 'ls'
-      puts 'listing files and folders in current directory'
+    #   puts 'listing files and folders in current directory'
     end
   when 'dir'
-    puts "adding folder #{line.split.last} to file system"
-    dir_sizes[line.split.last.to_sym]
+    # puts "adding folder #{line.split.last} to file system"
+    puts line.split(' ').first
+    dir_sizes[line.split(' ')[1..-1]]
   else
-    p "adding a file"
-    current_dir.each do |dir|
-      dir_sizes[dir.to_sym] += line.split(' ').first.to_i
-    end
+    # p "adding a file"
+    dir_sizes[current_dir[0..-1]] += line.split(' ').first.to_i
   end
 end
 
+# p current_dir
+
+# p dir_sizes
 p current_dir
-
 p dir_sizes
-
 p dir_sizes.sum { |k, v| v <= 100000 ? v : 0 }
---
+++ day07a.rb
@@ -0,0 +1,37 @@
+data = File.open('day07-input.txt')
+
+cwd = []
+dir_sizes = Hash.new { |h, k| h[k] = 0 }
+
+data
+  .map(&:chomp)
+  .map{_1.split(' ')}
+  .each do |line|
+    case line
+    in ['$', 'cd', '..']
+      cwd.pop
+    in ['$', 'cd', dir]
+      cwd << dir
+    in ['$', 'ls']
+    in ['dir', dirname]
+    in [size, filename]
+      cwd.length.times do |x|
+        dir_sizes[cwd[0..x]] += size.to_i
+      end
+    end
+  end
+
+p cwd
+p dir_sizes
+p dir_sizes
+  .inject(0) {|sum, (_, size)| size <= 100000 ? sum + size : sum}
+
+
+total = 70000000
+needed = 30000000
+free = total - dir_sizes[['/']]
+delete_at_least = needed - free
+
+# puts "Total: #{total}, Free: #{free}, Needed: #{needed}, Delete at least: #{delete_at_least}"
+
+# p dir_sizes.values.sort.find { |size| size > delete_at_least }
