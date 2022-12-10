1f3b2ba683be1443c3dfc233ae18153ac1d5951b
JudeLawrence <judesmail@gmail.com>
2022-12-10 12:10:33 +0400

    Complted day 7.

--- day07.rb
+++ day07.rb
@@ -2,6 +2,9 @@ file = File.open('day07-input.txt')
 
 file_system = {}
 dir_sizes = Hash.new { |h, k| h[k] = 0 }
+total_size = 70000000
+space_required = 30000000
+space_taken = 0
 
 current_dir = []
 
@@ -17,21 +20,29 @@ file.readlines.each do |line|
         current_dir.pop
       end
     when 'ls'
-    #   puts 'listing files and folders in current directory'
+    # puts 'listing files and folders in current directory'
     end
   when 'dir'
     # puts "adding folder #{line.split.last} to file system"
-    puts line.split(' ').first
+    # puts line.split(' ').first
     dir_sizes[line.split(' ')[1..-1]]
   else
     # p "adding a file"
-    dir_sizes[current_dir[0..-1]] += line.split(' ').first.to_i
+    space_taken += line.split(' ').first.to_i
+    current_dir.size.times do |x|
+      dir_sizes[current_dir[0..x]] += line.split(' ').first.to_i
+    end
   end
 end
 
-# p current_dir
+p dir_sizes.sum { |_k, v| v <= 100000 ? v : 0 }
 
-# p dir_sizes
-p current_dir
-p dir_sizes
-p dir_sizes.sum { |k, v| v <= 100000 ? v : 0 }
+p "Drive size: #{total_size}"
+p "Space taken: #{space_taken}"
+p "Space left: #{total_size - space_taken}"
+p "Space required for install: #{space_required}"
+
+
+folders_to_delete =  dir_sizes.select { |_k , v | v > space_required - (total_size - space_taken) }
+
+p folders_to_delete.sort_by { |key, value| value }.first
